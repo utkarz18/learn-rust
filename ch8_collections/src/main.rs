c150c5fa48c9eeb43cbb699d0cd3e3097f0bfbbd
Utkarsh Pandey <Utkarz18@gmail.com>
2024-09-19 00:24:00 +0530

    more vector operations

--- ch8_collections/src/main.rs
+++ ch8_collections/src/main.rs
@@ -10,4 +10,33 @@ fn main() {
     v.push(2);
 
     let x = vec![1, 2]; // macro way of creating vector
+
+    //accessing vectors
+    let item = v.get(0);
+    println!("Element at index 0 is : {}", item.unwrap());
+    let item = &x[1];
+    println!("Element at index 1 is : {}", item);
+
+    // borrowing rules
+    let y = &v[0];
+    // this cannot be done as y holds immutable reference and pushing new elements in vec can
+    // cause to deallocated old ref.
+    // v.push(3);
+    // println!("Element at index 0 is: {}", y);
+
+    // iterating over a vector
+    for item in &v {
+        print!("{item} ");
+    }
+
+    // modifying element while iterating, use dereference operator * before modifying elements
+    // make sure the vector is mutable 
+    for item in &mut v{
+        *item += 50
+    }
+    println!();
+    for item in &v {
+        print!("{item} ");
+    }
+    println!();
 }
