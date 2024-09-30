e6adfabe5d912676959a68a1c967b6f98be12bf0
Utkarsh Pandey <Utkarz18@gmail.com>
2024-09-30 23:04:18 +0530

    more on errors

--- ch9_errors/src/main.rs
+++ ch9_errors/src/main.rs
@@ -1,14 +1,23 @@
 use std::fs::File;
-use std::io::ErrorKind;
+use std::io::{Error, ErrorKind, Read};
 
 fn main() {
     let file_open_result = File::open("Hello_world.txt");
- 
+
     match &file_open_result {
         Ok(file) => println!("{file:?}"),
         Err(error) => match error.kind() {
             ErrorKind::NotFound => println!("File not found!"),
-            other_error => println!("Problem opening the file: {other_error:?}")
-        }
+            other_error => println!("Problem opening the file: {other_error:?}"),
+        },
     }
+
+    File::open("hello.txt").expect("File Not Found!");
+    read_file();
+}
+
+fn read_file() -> Result<String, Error> {
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
 }
